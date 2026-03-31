881e0d9079b49ce335f087e85191cc3ca560105d
auri-005 <auroragardisan@gmail.com>
2026-03-31 13:37:03 +0200

    err

--- slide1/es1slide.py
+++
@@ -1,7 +0,0 @@
-
-time = 538
-ore= int(time/60)
-resto = time%60
-
-print(f"ore: {ore}, minuti: {resto}")
-
--- slide1/es2slide.py
+++
@@ -1,7 +0,0 @@
-
-num = input("scegli un numero intero")
-num = int(num)
-quadrato = num**2  #print(f"{num**2}, {num**3}") oppure print(quadrato(n=2)) print(cubo(n=3))
-cubo = num**3
-
-print(f"quadrato : {quadrato} cubo : {cubo}")
