e4f2aeb44d93b25fe9090349186525b483d9332f
auri-005 <auroragardisan@gmail.com>
2026-04-12 11:23:20 +0200

    esercitazioni

--- esercitazioni/es4.py
+++ esercitazioni/es4.py
@@ -4,115 +4,107 @@ class CSVTimeSeriesFile():
     def __init__(self, name):
         self.name = name
         try:
-            file = open(self.name,"r")
-        except:
-            raise ExamException("Errore: impossibile aprire il file")
-        try:
-            file.read()
+            file = open(self.name, "r")
             file.close()
         except:
-            raise ExamException("Errore: il file è vuoto o non continene dati validi")
-    def get_data(self, nome_paese):
-        dat_temp = []
+            raise ExamException("Errore: impossibile aprire il file")
+    def get_data(self, country_name):
         file = open(self.name, "r")
+        data = []
+
         first_line = True
         for line in file:
             line = line.strip()
 
-            #salto solo l'intestazione
+            #salto l'intestazione, la prima riga
             if first_line:
-                first_line = False
+                first_line= False
                 continue
-
-            #se c'è una riga vuota la salto
-            if not line:
+            #se c'è una riga vuota la salto 
+            if not line :
                 continue
             #divido la riga in 'colonne'
-            elem = line.split(",")
-            #se non contiene almeno 4 colonne vado alla prossima
-            if len(elem)<5:
+            elem = line.split(";")
+            #controllo che ci siano almeno 4 colonne di elem:
+            if len(elem) < 4:
                 continue
-
+            #seleziono come se fosse un array i dati che mi servono
             date = elem[0]
             temp = elem[1]
-            country = elem[4]
-
-            if temp == "":
+            incert = elem[2]
+            country = elem[3]
+            #i miei dati se non soddisfano le condizioni vengono saltati e si prova con la riga seguente 
+            if temp == "" or incert == "" or country == "" or temp == "":
                 continue
             try:
                 temp = float(temp)
+                incert = float(incert)
             except:
                 continue
-            if country == nome_paese:
-                dat_temp.append([date,temp])
-        if dat_temp == []:
-            raise ExamException("Errore: il nome del paese non è presente nel file")
-        file.close()
-        return dat_temp
-    def compute_variations(self, time_series_1, time_series_2, first_year, last_year):
-        #estrarre le temperature per ciasciun anno
-        if first_year > last_year:
-            raise ExamException("Errore: intervallo di anni non valido")
-        
-        temp_ann1 = {}
-        temp_ann2 = {}
-        for x in time_series_1:
-            data1 = x[0]
-            temp1 = x[1]
-
-            data1 = int(data1[0:4])
-            if temp1 == "":
-                continue
-            if data1 not in temp_ann1:
-                temp_ann1[data1] = []
-            temp_ann1[data1].append(temp1)
-        
-        for y in time_series_2:
-            data2 = y[0]
-            temp2 = y[1]
             
-            data2 = int(data2[0:4])
-            if temp2 == "":
+            if incert >= 5:
                 continue
-            if data2 not in temp_ann2:
-                temp_ann2[data2] = []
-            temp_ann2[data2].append(temp2)
-        
-        #calcolare la media annuale per ogni anno nell'intervallo dato
-        #prima serie temporale
-        media_ann1 = {}
-        for year1, list_temp in temp_ann1.items():
-            if first_year <= year1 <=last_year:
-                media = sum(list_temp)/len(list_temp)
-                media_ann1[year1] = media
-        
-        #seconda serie temporale
-        media_ann2 = {}
-        for year2, list_temp in temp_ann2.items():
-            if first_year <= int(year2) <=last_year:
-                media = sum(list_temp)/len(list_temp)
-                media_ann2[year2] = media
+            #se country è la stessa che mi viene data in input la aggiungo alla mia lista 
+            if country == country_name:
+                data.append([date, temp, incert])
+        #se la mia lista è vuota, quindi non ho nessun paese nei dati uguale a quello dato in input alzo un eccezione 
+        if data == []:
+            raise ExamException("Errore: il paese richiesto non è presente nel file")
+        #chiudo il file e ritorno la mia lista 
+        file.close()
+        return data
+    def compute_cons_variation_compare(self, time_series1, time_series2, year):
+        if not isinstance(year, int):
+            raise ExamException("Errore: anno non valido, deve essere intero")
+        #raggruppo temperatura e incertezza per anno e mese nei dizionari relativi alle due time_series
+        rag_anno1 = {}
+        #estrapolo tutti i dati che mi servono da time_series1
+        for elem1 in time_series1:
+            data1 = elem1[0]
+            temp1 = elem1[1]
+            inc1 = elem1[2]
+            year1 = int(data1[3:7])
+            month1 = int(data1[0:2])
+
+            #se l'anno è lo stesso richiesto in input aggiungo nel dizionario la chiave mese e i valori richiesti
+            if year1 == year:
+                rag_anno1[month1] = [temp1, inc1]
+        rag_anno2 = {}
+        #eseguo gli stessi identici passsaggi anche per time_series2
+        for elem2 in time_series2:
+            data2 = elem2[0]
+            temp2 = elem2[1]
+            inc2 = elem2[2]
+            year2 = int(data2[3:7])
+            month2 = int(data2[0:2])
+
+            if year2 == year:
+                rag_anno2[month2] = [temp2, inc2]
+        if rag_anno1 == {} or rag_anno2=={}:
+            raise ExamException("Errore: l'anno indicato non rientra nella copertura del dataset")
+        #variazioni tra mesi consecutivi, entrambi i mesi sono presenti in entrambi gli anni
         result = {}
-        for x in media_ann1:
-            if x in media_ann2:
-                result[str(x)] = media_ann2[x] - media_ann1[x]
+        for m in range(1,12):
+            if m in rag_anno1 and m in rag_anno2:
+                if m+1 in rag_anno1 and m+1 in rag_anno2:
+                    #calcolo per time_series1
+                    var1 = rag_anno1[m+1][0] - rag_anno1[m][0]
+                    max_inc1 = rag_anno1[m][1] + rag_anno1[m+1][1]
+                    #calcolo per time_series2
+                    var2 = rag_anno2[m+1][0] - rag_anno2[m][0]
+                    max_inc2 = rag_anno2[m][1] + rag_anno2[m+1][1]
 
-        """
-        oppure per calcolare le medie e le differenze: più veloce e 'corretto', spreco meno tempo:
-        for year in range(first_year, last_year + 1):  #eseguo direttamente i cicli sugli anni richiesti senza stare a controllare ogni anno ogni volta
-            if year in temp_ann1 and year in temp_ann2:
-                media1 = sum(temp_ann1[year]) / len(temp_ann1[year])
-                media2 = sum(temp_ann2[year]) / len(temp_ann2[year])
-                result[str(year)] = media2 - media1
-
-        """
-
+                    #confrontare le variazioni 
+                    var = var2 - var1
+                    max_inc = max_inc2 + max_inc1
+                    #salvare ii dati in un dizionario
+                    result[m] = (float(var), float(max_inc))
+        if result == {}:
+            raise ExamException("Errore: nessuna coppia di mesi valida per il confronto tra gli anni indicati")
         return result
-        
 
 
-
-time_series_file = CSVTimeSeriesFile(r"esercitazioni/1-4.csv")
-time_series_italy = time_series_file.get_data("Italy")
-#print(time_series_italy)
-print(time_series_file.compute_variations(time_series_file.get_data("Italy"), time_series_file.get_data("India"), 1990, 1993))
+ts_file = CSVTimeSeriesFile(r"esercitazioni/4.csv")
+t1 = ts_file.get_data("Zambia")
+t2 = ts_file.get_data("Sudan")
+print(ts_file.compute_cons_variation_compare(t1, t2, 1990))
